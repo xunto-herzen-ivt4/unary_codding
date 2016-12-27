ea536d26f24d27803ff6e06f5d1349b967dc8a20
Xunto <hunterdelyx1@gmail.com>
2016-12-28 02:00:15 +0300

    make tests useful

--- tests.py
+++ tests.py
@@ -3,8 +3,10 @@ import random
 
 for i in range(100):
     print(i, 'is started')
-    array = [random.randint(0, 100) for _ in range(200)]
+    array = [random.randint(0, 10) for _ in range(5)]
     encoded = encode(array)
     decoded = decode(encoded)
+    print(array)
+    print(decoded)
     assert array == decoded
     print(i, 'is completed')
